3d47bb199c9acf48e9031d8577bc4e53a07bc69c
Micheal <mykmicheals@gmail.com>
2023-04-14 23:55:43 +0100

    added gradient to btn

---
+++ src/components/BtnPurple.js
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const BtnPurple = (props) => {
+  return (
+    <TouchableOpacity>
+      <LinearGradient
+        colors={["#0B365B", "#124672"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        className="px-10 my-3 rounded-lg  h-12 flex items-center justify-center w-80"
+      >
+        <Text className="text-center align-center text-white">
+          {props.title}
+        </Text>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
+
+export default BtnPurple;
