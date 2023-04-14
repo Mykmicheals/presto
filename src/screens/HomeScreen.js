fde829d18e974739a268e8ea08cf242a2aca736c
Micheal <mykmicheals@gmail.com>
2023-04-14 23:56:05 +0100

    implemented safeareaview

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,44 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import React from "react";
 import barcode from "../assests/images/barcode.png";
+import { center, styles } from "../styles/HomeStyles";
+import BtnPurple from "../components/BtnPurple";
+import Table from "../components/Table";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Select BitCoin</Text>
+    <SafeAreaView className="flex-1 items-center mt-16 w-full bg-white">
+      <Text className="text-2xl mb-5">Select Bitcoin</Text>
       <View>
-        <Image source={barcode} />
+        <Image className="mb-3" source={barcode} />
       </View>
-      <View>
-        <Text>Btc Wallet Details</Text>
+      <View style={styles.container}>
+        <Text className="text-lg">BTC Wallet Details</Text>
         <Text>3nofvnodslfihvolfishosfvonsXd</Text>
-        <TouchableOpacity>
-          <Text>Click to copy</Text>
-        </TouchableOpacity>
-        <Text>
+        <BtnPurple title="Click to copy" />
+        <Text className="mx-9 mb-4 text-gray-500">
           The address and QR barcode are yours. you can recieve bitcoins into it
           and please provide proof.
         </Text>
-        <Text>Kindly upload Proof</Text>
-        <Text>BTC Rates Table</Text>
+        <Text className="text-left mb-2">Kindly upload Proof</Text>
+        <View className="flex items-center justify-center">
+          <View
+            className={`${center} border-2 h-12  border-dotted border-gray-500 w-4/5 mb-8 px-24`}
+          >
+            <Text className={` text-center text-gray-500`}>Upload Proof</Text>
+          </View>
+        </View>
+
+        <Text className='mb-4'>BTC Rates Table</Text>
+        <Table />
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
