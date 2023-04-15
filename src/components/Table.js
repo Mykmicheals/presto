fef92b9ab9708470dec00d6c154191ae6581fbdc
Micheal <mykmicheals@gmail.com>
2023-04-15 11:25:07 +0100

    refactored the table code

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,37 +2,31 @@ import { View, Text } from "react-native";
 import React from "react";
 import { center } from "../styles/HomeStyles";
 
-var tableData = [
+const tableData = [
   { value: "Less than 100", rate: 520 },
   { value: "100 to 1000", rate: 535 },
   { value: "Above 1,000", rate: 550 },
 ];
 
 const Table = () => {
+  const renderTableData = (data, key) => (
+    <View>
+      <Text className="mb-2">{key}</Text>
+      {data.map((each) => (
+        <Text key={each.value} className="mb-3 text-blue-900">
+          {each[key.toLowerCase()]}
+        </Text>
+      ))}
+    </View>
+  );
+
   return (
     <View className={`${center} flex flex-row gap-20`}>
-      <View>
-        <Text className="mb-2">Value</Text>
-        {tableData.map((each) => {
-          return (
-            <Text key={each.name} className="mb-3 text-blue-900">
-              {each.value}
-            </Text>
-          );
-        })}
-      </View>
-      <View>
-        <Text className="mb-2">Rate</Text>
-        {tableData.map((each) => {
-          return (
-            <Text key={each.name} className="mb-3 text-blue-900">
-              {each.rate}
-            </Text>
-          );
-        })}
-      </View>
+      {renderTableData(tableData, "Value")}
+      {renderTableData(tableData, "Rate")}
     </View>
   );
 };
+
 
 export default Table;
