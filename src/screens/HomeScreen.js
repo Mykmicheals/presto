908296d03f88ede459169f2a8f16ede82ffcf245
Micheal <mykmicheals@gmail.com>
2023-04-15 10:11:13 +0100

    added font weight

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,4 @@
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  SafeAreaView,
-} from "react-native";
+import { View, Text, Image, SafeAreaView } from "react-native";
 import React from "react";
 import barcode from "../assests/images/barcode.png";
 import { center, styles } from "../styles/HomeStyles";
@@ -15,28 +9,29 @@ import UploadInput from "../components/UploadInput";
 const HomeScreen = () => {
   return (
     <SafeAreaView className="flex-1 items-center mt-16 w-full bg-white">
-      <Text className="text-2xl mb-5 font-quicksand">Select Bitcoin</Text>
+      <Text style={styles.styleHeader} className="text-2xl mb-4">
+        Select Bitcoin
+      </Text>
       <View>
         <Image className="mb-3" source={barcode} />
       </View>
       <View style={styles.container}>
-        <Text className="text-lg">BTC Wallet Details</Text>
-        <Text>3nofvnodslfihvolfishosfvonsXd</Text>
+        <Text className="text-lg font-[400]">BTC Wallet Details</Text>
+        <Text className="font-[400]">3nofvnodslfihvolfishosfvonsXd</Text>
         <BtnPurple title="Click to copy" />
 
         <Text className="mx-9 mb-4 text-gray-500 text-xs">
           The address and QR barcode are yours. you can recieve bitcoins into it
           and please provide proof.
         </Text>
-
-        <Text className="text-left mb-2">
-          Kindly upload Proof
-        </Text>
-
-        <UploadInput />
-
-        <Text className="mb-4">BTC Rates Table</Text>
-        <Table />
+        <View>
+          <Text className="items-left mb-2">Kindly upload Proof</Text>
+          <UploadInput />
+        </View>
+        <View className="w-80">
+          <Text className="items-left mb-4">BTC Rates Table</Text>
+          <Table />
+        </View>
       </View>
     </SafeAreaView>
   );
