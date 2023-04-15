39610694bbc47b60cf4580c871bb7a818dadfbf1
Micheal <mykmicheals@gmail.com>
2023-04-15 13:16:33 +0100

    final entry file

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-import * as Font from "expo-font";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
-import quickSandRegular from "./src/assests/fonts/Quicksand-Regular.ttf";
-
-async function loadFonts() {
-  await Font.loadAsync({
-    "Quicksand-Regular": quickSandRegular,
-  });
-}
 
 export default function App() {
-  useEffect(() => {
-    loadFonts();
-  }, []);
   return (
     <View style={styles.container}>
       <HomeScreen />
